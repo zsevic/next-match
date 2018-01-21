b928e8077efb3529f81180804aa69025f96ae60b
zsevic <zsevic95@gmail.com>
2018-01-21 22:54:16 +0100

    :sparkles: fix bug with team-b and move to BeautifulSoup for scraping

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
-import gi,signal,os,notify2,urllib
-from htmldom import htmldom
-from urllib.request import urlopen, Request
-gi.require_version('Gtk','3.0')
+import gi, signal, os, notify2, urllib
+from urllib.request import urlopen
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk
-gi.require_version('AppIndicator3','0.1')
+gi.require_version('AppIndicator3', '0.1')
 from gi.repository import AppIndicator3 as appindicator
 
-APPINDICATOR_ID='nextmatch'
-URL_FOOTBALL='http://int.soccerway.com/teams/serbia/fk-crvena-zvezda-beograd/1942'
-URL_BASKETBALL='http://www.scoresway.com/?sport=basketball&page=team&id=150'
+from bs4 import BeautifulSoup
+
+APPINDICATOR_ID = 'nextmatch'
+URL_FOOTBALL = 'http://int.soccerway.com/teams/serbia/fk-crvena-zvezda-beograd/1942'
+URL_BASKETBALL = 'http://www.scoresway.com/?sport=basketball&page=team&id=150'
 
 def main():
-    indicator=appindicator.Indicator.new(APPINDICATOR_ID,os.path.abspath('./images/icon.png'),appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('./images/icon.png'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
     notify2.init(APPINDICATOR_ID)
     gtk.main()
 
 def build_menu():
-    menu=gtk.Menu()
-    item_football=gtk.MenuItem('Next football match')
-    item_football.connect('activate',football)
-    item_basketball=gtk.MenuItem('Next basketball match')
-    item_basketball.connect('activate',basketball)
-    item_quit=gtk.MenuItem('Quit')
-    item_quit.connect('activate',quit)
+    menu = gtk.Menu()
+    item_football = gtk.MenuItem('Next football match')
+    item_football.connect('activate', football)
+    item_basketball = gtk.MenuItem('Next basketball match')
+    item_basketball.connect('activate', basketball)
+    item_quit = gtk.MenuItem('Quit')
+    item_quit.connect('activate', quit)
     menu.append(item_football)
     menu.append(item_basketball)
     menu.append(item_quit)
@@ -32,11 +33,11 @@ def build_menu():
     return menu
 
 def football(_):
-    fb=notify2.Notification("<b>Next football match</b>",next_match(URL_FOOTBALL))
+    fb = notify2.Notification("<b>Next football match</b>", next_match(URL_FOOTBALL))
     fb.show()
 
 def basketball(_):
-    bb=notify2.Notification("<b>Next basketball match</b>",next_match(URL_BASKETBALL))
+    bb = notify2.Notification("<b>Next basketball match</b>", next_match(URL_BASKETBALL))
     bb.show()
 
 def quit(_):
@@ -44,36 +45,41 @@ def quit(_):
     gtk.main_quit()
 
 def next_match(url):
-    dom=htmldom.HtmlDom()
-    resp=urlopen(url)
-    html=resp.read().decode('utf-8')
-    dom=dom.createDom(html)
-    win_length=dom.find(".result-win").length()
-    draw_length=dom.find(".result-draw").length()
-    loss_length=dom.find(".result-loss").length()
-    length=win_length+draw_length+loss_length
-    fixture=dom.find(".matches > tbody").children().eq(length)
-    if(fixture==None):
+    page = urlopen(url).read()
+    soup = BeautifulSoup(page,'html.parser')
+    past_matches = soup.findAll('a', {
+        'class':[
+            'result-win',
+            'result-loss',
+            'result-draw'
+        ]}
+    )
+    next_round = len(past_matches)
+    table = soup.find('table', {'class': 'matches'})
+    table_body = table.find('tbody')
+    matches = table_body.findAll('tr')
+    next_one = matches[next_round]
+    if next_one == None:
         return "There is no match in the schedule"
-    return parseHtml(fixture.html())
+    return parseHtml(str(next_one))
 
 def parseHtml(html):
-    res=""
-    dom=htmldom.HtmlDom()
-    dom=dom.createDom(html)
-    time=dom.find(".timestamp").text()
-    if(len(time)==0):
-        day=dom.find(".match_timestamp").text()
-        date=dom.find(".full-date").text()
-        res+=day
-        res+=date
-    else:
-        res+=time
-    team_a=dom.find(".team-a").text()
-    team_b=dom.find(".team-b").text()
-    res+="".join([team_a.strip()," - ",team_b.strip()])
+    res = ''
+    match_html = BeautifulSoup(html, 'html.parser')    
+    day = match_html.find('td', {'class': 'day'}).text
+    date = match_html.find('td', {'class': 'full-date'}).text
+    res += '\n'.join([day,date])
+    if(match_html.find('td', {'class': 'score-time'})):
+        time = match_html.find('td', {'class': 'score-time'}).text
+        res += time
+    
+    team_a = match_html.find('td', {'class': 'team-a'})
+    team_a = team_a.find('a').get('title')
+    team_b = match_html.find('td', {'class': 'team-b'})
+    team_b = team_b.find('a').get('title')
+    res += ' '.join([team_a.strip(), '-', team_b.strip()])
     return res
 
-if __name__=="__main__":
+if __name__ == "__main__":
     signal.signal(signal.SIGINT,signal.SIG_DFL)
     main()
